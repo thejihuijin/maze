a009e8037a050abf8611943de4d47098a4f7e14b
jmchan100 <jmchan100@gmail.com>
2014-08-09 16:53:00 -0700

    Buggy autogenerate marios

--- goombaDodger/js/move.js
+++ goombaDodger/js/move.js
@@ -4,7 +4,8 @@ function init(){
     imgObj = document.getElementById('myImage');
     imgObj.style.position= 'absolute'; 
     imgObj.style.left = '0px';
-    imgObj.style.bottom = '0px'; 
+    imgObj.style.bottom = '0px';
+    showRock('img/mario.png', 'rockimg');
 }
 function moveRight(){
     if (position < 2) {
