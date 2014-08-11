abdb1f9a6aefa903a5d0328cd427e5faece8e8dc
Jihui Jin <thejihuijin@gmail.com>
2014-08-11 13:43:24 -0700

    made movable class

--- goombaDodger/js/move.js
+++ goombaDodger/js/move.js
@@ -1,12 +1,13 @@
 var imgObj = null;
 var position = 0;
+/* // moved to css file for goombaDodger
 function init(){
     imgObj = document.getElementById('myImage');
     imgObj.style.position= 'absolute'; 
     imgObj.style.left = '0px';
     imgObj.style.bottom = '0px';
-}
-function moveRight(){
+}*/
+/*function moveRight(){
     if (position < 2) {
         imgObj.style.left = parseInt(imgObj.style.left) + 100 + 'px';
         position++;
@@ -17,9 +18,46 @@ function moveLeft(){
         imgObj.style.left = parseInt(imgObj.style.left) - 100 + 'px';
         position--;
     }
+}*/
+
+function moveRight() {
+    // jquery to grab those of the class, moveable
+    $(".moveable").each(function(index, element) {
+        // element is a node with the desired class name
+        if (checkRight(element)) {
+            // initializes the style.left field so it can be changed
+            element.style.left = document.defaultView.getComputedStyle(element).left;
+            element.style.left = parseInt(element.style.left) + 100 + 'px';
+        }
+    });
+}
+function checkRight(element) {
+    var pdiv = element.parentNode;
+    var pwidth = parseInt(pdiv.style.width);
+    var ePosition = parseInt(element.width) + element.offsetLeft;
+    return pwidth > ePosition; 
 }
 
-window.onload = init;
+function moveLeft() {
+    // jquery to grab those of the class, moveable
+    $(".moveable").each(function(index, element) {
+        // element is a node with the desired class name
+        if (checkLeft(element)) {
+            // initializes the style.left field so it can be changed
+            element.style.left = document.defaultView.getComputedStyle(element).left;
+            element.style.left = parseInt(element.style.left) - 100 + 'px';
+        }
+    });
+}
+function checkLeft(element) {
+    return element.offsetLeft > 0; 
+}
+
+
+
+
+
+//window.onload = init;
 document.addEventListener('keydown', function(event) {
     var arrow = false;
     switch(event.which) {
@@ -29,7 +67,6 @@ document.addEventListener('keydown', function(event) {
         break;
 
         case 39: // right
-        //alert('right clicked');
         moveRight();
         arrow = true;
         break;
